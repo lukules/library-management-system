dc1abbdad6f486515e924537a0e9900f9962d0e6
kules <kulesza.lukasz@icloud.com>
2026-09-23 17:45:33 +0200

    Genre modal created

--- backend/untitled/src/main/java/org/example/modal/Genre.java
+++ backend/untitled/src/main/java/org/example/modal/Genre.java
@@ -1,5 +1,57 @@
 package org.example.modal;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Genre code is required")
+    private String code;
+
+    @NotBlank(message = "Genre name is required")
+    private String name;
+
+    @Size(max = 500, message = "Depscription must not exceed 500 characters")
+    private String description;
+
+    @Min(value = 0, message = "Display order cannot be negative")
+    private Integer displayOrder = 0;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @ManyToOne
+    private Genre parentGenre;
+
+    @OneToMany
+    private List<Genre> subGenres = new ArrayList<Genre>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
 }
