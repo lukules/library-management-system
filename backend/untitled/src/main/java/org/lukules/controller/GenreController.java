d83ce80d2cd94fae9eb9e743392bdc3cea03395a
kules <kulesza.lukasz@icloud.com>
2026-09-23 18:14:19 +0200

    Added endpoint to GenreController

--- backend/untitled/src/main/java/org/lukules/controller/GenreController.java
+++ backend/untitled/src/main/java/org/lukules/controller/GenreController.java
@@ -6,10 +6,12 @@ import org.lukules.service.GenreService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/genres")
 public class GenreController {
     private final GenreService genreService;
 
